65a4cdf5e6b2239a915db6bbc99d80e62a7c1cb4
lee <lsljw@daum.net>
2024-01-06 13:35:19 +0900

    4th

--- js/common.js
+++ js/common.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
     var $sectionTop=0;
     var headerHeight;
+
+    // banner 섹션 처리
+    var bannerTop, bannerBoxHeight;
+    var $vertical=0, bannerPosition=0;
+    var $lastHeight, timeOut, $bannerContainer, windowHeight, wheelStart;
+
     // /////////////// 초기값 설정 /////////////////
     function init(){
         windowHeight=$(window).height();
         headerHeight=$("header").height();
-        console.log("window창의 높이 : "+windowHeight)
-        console.log("header 높이 : "+headerHeight)
+        // console.log("window창의 높이 : "+windowHeight)
+        // console.log("header 높이 : "+headerHeight)
         
         $("section").css({
             height:windowHeight-(headerHeight)
         })
+
+        // banner 초기화
+        bannerTop=$("#banner-detail").offset().top;
+        $bannerContainer=$(".banner-detail-container").height();
+        bannerBoxHeight=$(".box-wrap").height();
+        $vertical=150;
+        $lastHeight=bannerBoxHeight-$bannerContainer;
+        // console.log("bannerBox : "+bannerBoxHeight)
+        // console.log("$vertical : "+$vertical);
+        // console.log("마지막 위치 : "+$lastHeight)
+        // console.log("banner-deail-container : "+$(".banner-detail-container").height())
         
     }
     init();
+
+    //  재설정
+    $(window).resize(function(){
+        init();
+    })
     // ////////////// 네비 버튼 //////////////////
     var $navBool=true;
     $(".navBt").click(function(){
@@ -32,14 +54,14 @@ $(document).ready(function(){
     var $position=0;
     $(".nav-list a").each(function(index){
         $(this).click(function(){
-            console.log("$(this.hash).prev().height() : "+$(this.hash).prev().height())
+            // console.log("$(this.hash).prev().height() : "+$(this.hash).prev().height())
             if($position<=index){
 
-                console.log("다음요소, 현재요소")
+                // console.log("다음요소, 현재요소")
                 $hash=$(this.hash).offset().top;
                 
             }else{
-                console.log("이전요소")
+                // console.log("이전요소")
                 if(index<=0){
                     $hash=0
                 }else{
@@ -71,22 +93,73 @@ $(document).ready(function(){
         // console.log("index : "+index)
         $(window).scroll(function(){
             var $scrollTop=$(this).scrollTop()
-            console.log("스크롤 탑 : "+$scrollTop)
+            // console.log("스크롤 탑 : "+$scrollTop)
             $sectionTop=$(".section").eq(index).offset().top
-            console.log("섹션 탑 : "+$sectionTop)
+            // console.log("섹션 탑 : "+$sectionTop)
             if($scrollTop+500>=$sectionTop){
-               console.log("큽니다.")
-               console.log("index : "+index)
+            //    console.log("큽니다.")
+            //    console.log("index : "+index)
                $position=index;
                $aniIndex=index;
-               console.log("$position : "+$position)
+            //    console.log("$position : "+$position)
                $active($position);
                $animation($aniIndex)
-            }else{
-                $aniIndex=index-1;
             }
         })
     })
+
+    //  box-wrap wheel
+    function boxWheel(){
+        $(".box-wrap").on("DOMMouseScroll mousewheel", function(e){
+            clearTimeout(timeOut);
+            timeOut = setTimeout(function(){
+                if(e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0 ){
+                    // console.log("올리고 있음")
+                    // console.log("bannerPosition : "+bannerPosition)
+                    if(bannerPosition>0){
+
+                        bannerPosition=bannerPosition-$vertical;
+                    }else{
+                        console.log("올림 끝남")
+                        $(".box-wrap").off()
+                        wheelStart=bannerPosition;
+                    }
+
+                }else{
+                    // console.log("내리고 있음")
+                    if(bannerPosition<$lastHeight){
+                        bannerPosition=bannerPosition+$vertical; 
+                        // console.log("bannerPosition : "+bannerPosition)
+                       
+                    }else{
+                        console.log("내림 끝남")
+                        $(".box-wrap").off()
+                        wheelStart=bannerPosition;   
+                    }
+                    
+                }
+                $(".box-wrap").stop().animate({
+                    top:-bannerPosition
+                },800)
+            },100)
+            return false;
+        })
+        
+    }
+
+    boxWheel();
+
+    $(window).scroll(function(){
+        var $scrollTop=$(this).scrollTop()
+        if( wheelStart<=0 || wheelStart>=$lastHeight){
+            console.log("wheelStart : "+wheelStart)
+            // $(".box-wrap").off()
+            boxWheel()
+        }
+
+    })
+
+    // ////////////////////////////////
     var $aniIndex=0;
     function $animation(){
         $(".section").eq($aniIndex-1).find(".about-container").removeClass("aboutActive");
